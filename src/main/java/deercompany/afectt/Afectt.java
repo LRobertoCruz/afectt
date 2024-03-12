1df20350e095278dff0104c15b26c238e59a1298
LRobertoCruz <gitEvakOrunn@hotmail.com>
2024-03-12 12:18:38 -0300

    first commit

---
+++ src/main/java/deercompany/afectt/Afectt.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package deercompany.afectt;
+
+import deercompany.ventana.InicioSesion;
+
+/**
+ *
+ * @author Luis Cruz
+ */
+public class Afectt {
+
+    public static void main(String[] args) {
+        InicioSesion inicio = new InicioSesion();
+        inicio.setVisible(true);
+    }
+}
